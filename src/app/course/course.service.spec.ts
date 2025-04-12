2db3784d3fbdf376eecab0d3ed09f026975c83b5
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-04-12 09:37:53 -0500

    Probar el componente

--- src/app/course/course.service.spec.ts
+++ src/app/course/course.service.spec.ts
@@ -2,15 +2,17 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CourseService } from './course.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('Service: Course', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CourseService]
-    });
-  });
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [CourseService]
+   });
+ });
 
-  it('should ...', inject([CourseService], (service: CourseService) => {
-    expect(service).toBeTruthy();
-  }));
+ it('should ...', inject([CourseService], (service: CourseService) => {
+   expect(service).toBeTruthy();
+ }));
 });
