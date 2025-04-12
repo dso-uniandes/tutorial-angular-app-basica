e14d1004fc9c104ce2b06e935d6757ae937151e9
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-04-12 09:02:20 -0500

    Modifica el controlador

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { dataCourses } from './dataCourses';
 
 @Component({
   selector: 'app-course',
@@ -10,8 +11,11 @@ export class CourseComponent implements OnInit {
 
   courses: Array<Course> = [];
   constructor() { }
-
+  getCourseList(): Array<Course> {
+    return dataCourses;
+  }
   ngOnInit() {
+    this.courses = this.getCourseList();
   }
 
 }
