2ef47d985a54498c48aa2481aa394e738631ad86
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-04-12 09:00:19 -0500

    Definir el modelo

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-course',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  courses: Array<Course> = [];
   constructor() { }
 
   ngOnInit() {
